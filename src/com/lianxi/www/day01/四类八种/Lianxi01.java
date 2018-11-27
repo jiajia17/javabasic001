8e51528cffa667384499e0a83087803f60c1b45c
Administrator <Administrator@SC-201707181923>
2018-11-27 22:24:16 +0800

    这次提交代码主要做了哪些工作

--- src/com/lianxi/www/day01/四类八种/Lianxi01.java
+++ src/com/lianxi/www/day01/四类八种/Lianxi01.java
@@ -3,6 +3,9 @@ package com.lianxi.www.day01.Àƒ¿‡∞À÷÷;
 public class Lianxi01 {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		
 	}
 
 }
